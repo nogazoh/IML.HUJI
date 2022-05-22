10893fe047a3a1007fedef0798eee61c8240fcaf
Noga Zohar <nogaz3388@gmail.com>
2022-05-22 15:00:20 +0300

    2205

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -27,14 +27,26 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
         and potentially additional arguments. The function returns the score for given input.
 
     cv: int
-        Specify the number of folds.
+        Specify the number of folds. יעני לכמה אני מחלקת את זה
 
     Returns
     -------
     train_score: float
-        Average train score over folds
+        Average train score over folds // loss during training
 
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    X_sub_arrays = np.split(X, cv)
+    y_sub_arrays = np.split(y, cv)
+    wo_folds = []
+    folds = []
+    for i in range(cv):
+        cur_X, cur_y = X, y
+        cur_X = np.delete(cur_X, X_sub_arrays[i])
+        cur_y = np.delete(cur_y, y_sub_arrays[i])
+        model = estimator.fit(cur_X, cur_y)
+        y_pred = model.predict(X)
+        folds.append(scoring(X_sub_arrays[i], y_sub_arrays[i], cv))
+        wo_folds.append(scoring(cur_X, y_pred, cv))
+    return np.average(wo_folds), np.average(folds)
